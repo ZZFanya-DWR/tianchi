4b5d86f13c0811c3faa4853c3676427a50133d0e
lvfan <zzfanya@qq.com>
2022-05-02 21:56:22 +0800

    add

--- test.py
+++ test.py
@@ -1,3 +1,7 @@
 import numpy as np
 
-a = np.random.random((3,256))
+a = np.random.random((3,256)
+b = np.zeros((3,2))
+c = np.ones((4,3))
+d = np.random.randint((2,26))
+e = np.array([1,2,3,4,6,7])
