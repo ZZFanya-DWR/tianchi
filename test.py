0bd49617bb64e0f54a175e6bb52d96448576c284
lvfan <zzfanya@qq.com>
2022-05-02 22:10:32 +0800

    test

--- test.py
+++ test.py
@@ -5,6 +5,7 @@ b = np.zeros((3,2))
 c = np.ones((4,3))
 d = np.random.randint((2,26))
 e = np.array([1,2,3,4,6,7])
-d = np.random.randint((1,26))
+d = np.random.randint((2,26))
 e = np.array([1,2,3,4])
 #add by two head and main glasses 
+#delete by main
